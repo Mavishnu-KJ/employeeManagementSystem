0ffd39494d46f11bbe6b7c32218858590fb10922
MAVISHNU KJ <kjmavishnu@gmail.com>
2026-03-13 12:34:29 +0530

    Added some more proper validations

--- src/main/java/com/example/employeeManagementSystem/model/dto/EmployeeRequestDto.java
+++ src/main/java/com/example/employeeManagementSystem/model/dto/EmployeeRequestDto.java
@@ -3,13 +3,14 @@ package com.example.employeeManagementSystem.model.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record EmployeeRequestDto (
-        @NotBlank String name,
-        @Positive Integer salary,
+        @NotBlank String name, //@NotBlank covers null check as well for String
+        @NotNull @Positive Integer salary, //@NotNull should be used since it is Integer. If we us @NotBlank, it will throw unexpected error even for proper input values
         String department,
-        @Email String email
+        @NotBlank @Email String email //@NotBlank covers null check as well for String
 ){
 
 }
